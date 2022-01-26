dff9f64f798daa83a7f57ba7100f0561219eb4eb
Balaram Annadi <balaram.annadi@gmail.com>
2022-01-26 15:53:45 +0530

    update file

--- Shell-Scripting/roboshop-project/roboshop.sh
+++ Shell-Scripting/roboshop-project/roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -f components/$1.sh ]; then
-  bash components.$1.sh
+  bash components/$1.sh
 else
   echo -e "\-e[1;31mInvalid Input\e[0m"
   echo -e "\e[1;33mAvailable Inputs -cart|catalogue|dispatch|frontend|mongodb|mysql|payment|rabbitmq|redis|roboshop|shopping\e[0m"
